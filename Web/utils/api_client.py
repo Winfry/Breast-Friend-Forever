59fddd956dff75a562b82df765b8f8306baf22f2
Winfry <Winfrynyarangi@gmail.com>
2025-10-21 16:12:35 +0300

    breastaware_layout.json.txt

--- Web/utils/api_client.py
+++ Web/utils/api_client.py
@@ -2,7 +2,7 @@
 import requests
 
 class ApiClient:
-    def __init__(self, backend_url="http://127.0.0.1:8000"):
+    def __init__(self, backend_url="http://127.0.0.1:8001"):
         self.backend_url = backend_url
 
     def health_check(self):
